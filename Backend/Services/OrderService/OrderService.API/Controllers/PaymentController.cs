b2d7d277833a968921efc79c1033ebdb0e455624
kamilla <kamilla.melikava@gmail.com>
2025-04-01 09:17:42 +0300

    Remove comments

--- Backend/Services/OrderService/OrderService.API/Controllers/PaymentController.cs
+++ Backend/Services/OrderService/OrderService.API/Controllers/PaymentController.cs
@@ -19,7 +19,6 @@ namespace OrderService.API.Controllers
             _logger = logger;
         }
 
-        //[Authorize(Policy = "Client")]
         [HttpPost]
         public async Task<IActionResult> CreateCheckoutSession(Guid orderId, CancellationToken cancellationToken)
         {
